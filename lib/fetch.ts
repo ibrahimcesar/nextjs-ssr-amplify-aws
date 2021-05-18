9b87d39400b236e75d2ea64f3bbe60e1789f1712
Ibrahim Cesar <ibrahim.souza@nexojornal.com.br>
2021-05-18 19:47:10 -0300

    chore(amplify): added amplify config

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,7 +1,7 @@
 import axios from 'redaxios'
 
 export async function getPokemons(n?: number) {
-  const pokemons = n || 1118
+  const pokemons = n || 3000
   const {data: response} = await axios(`https://pokeapi.co/api/v2/pokemon?limit=${pokemons}`);
   return response
 }
